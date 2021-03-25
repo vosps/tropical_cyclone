859f02de650a57efa530b25454edb4f003b9c256
lucy harris <71592519+ljharris23@users.noreply.github.com>
2021-03-25 15:51:41 +0000

    filters
    Former-commit-id: 4e1f039f40c9c1ef94480fb729150cc5c3aeecfe

--- dsrnngan/models.py
+++ dsrnngan/models.py
@@ -1,10 +1,9 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, concatenate, Flatten, Conv2D, UpSampling2D
-from tensorflow.keras.layers import GlobalAveragePooling2D, Concatenate
+from tensorflow.keras.layers import GlobalAveragePooling2D
 
 from blocks import residual_block, const_upscale_block
-from layers import ReflectionPadding2D
 
 def generator(era_dim=(10,10,9), const_dim=(250,250,2), noise_dim=(10,10,8), filters=64, conv_size=(3,3), stride=1, relu_alpha=0.2, norm=None, dropout_rate=None):
     
@@ -35,7 +34,7 @@ def generator(era_dim=(10,10,9), const_dim=(250,250,2), noise_dim=(10,10,8), fil
     
     print(f"Shape before upsampling: {generator_output.shape}")
     ## Upsampling residual blocks 
-    block_channels = [64, 32]
+    block_channels = [2*filters, filters]
     ## There are 2 items in block_channels so we upsample 2 times
     ## Upsampling size is hardcoded at (5,5)
     for i, channels in enumerate(block_channels):
@@ -167,7 +166,7 @@ def discriminator(era_dim=(10,10,9), const_dim=(250,250,2), nimrod_dim=(250,250,
     ##encode inputs using residual blocks
     ##stride of 5 means hi-res inputs are downsampled
     ##there are 2 items in block_channels so we pass through 2 residual blocks
-    block_channels = [32, 64]
+    block_channels = [filters, 2*filters]
     for (i,channels) in enumerate(block_channels):
         lo_res_input = residual_block(lo_res_input, filters=channels, conv_size=conv_size, stride=stride, relu_alpha=relu_alpha, norm=norm, dropout_rate=dropout_rate)
         print(f"Shape of lo-res input after residual block: {lo_res_input.shape}")
@@ -182,8 +181,8 @@ def discriminator(era_dim=(10,10,9), const_dim=(250,250,2), nimrod_dim=(250,250,
     print(f"Shape after concatenate: {disc_input.shape}")
     
     ##encode in residual blocks
-    disc_input = residual_block(disc_input, filters=64, conv_size=conv_size, stride=stride, relu_alpha=relu_alpha, norm=norm, dropout_rate=dropout_rate)
-    disc_input = residual_block(disc_input, filters=64, conv_size=conv_size, stride=stride, relu_alpha=relu_alpha, norm=norm, dropout_rate=dropout_rate)
+    disc_input = residual_block(disc_input, filters=filters, conv_size=conv_size, stride=stride, relu_alpha=relu_alpha, norm=norm, dropout_rate=dropout_rate)
+    disc_input = residual_block(disc_input, filters=filters, conv_size=conv_size, stride=stride, relu_alpha=relu_alpha, norm=norm, dropout_rate=dropout_rate)
     print(f"Shape after residual block: {disc_input.shape}")
     print('End of second residual block')
 
