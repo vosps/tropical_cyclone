3778fb53d32d659bf5df5a89275056ce07641ab1
Matthew Chantry <matthew.chantry@physics.ox.ac.uk>
2021-03-25 09:41:55 +0000

    increasing number of filters
    Former-commit-id: 153b5f4c317e3b8c4bf4656bf4a5935a7c6f6c03

--- dsrnngan/models.py
+++ dsrnngan/models.py
@@ -24,7 +24,7 @@ def initial_state_model(num_preproc=3):
     )
 
 
-def generator(era_dim=(10,10,9), const_dim=(250,250,2), noise_dim=(10,10,8), filters=32, conv_size=(3,3), stride=1, relu_alpha=0.2, norm=None, dropout_rate=None):
+def generator(era_dim=(10,10,9), const_dim=(250,250,2), noise_dim=(10,10,8), filters=64, conv_size=(3,3), stride=1, relu_alpha=0.2, norm=None, dropout_rate=None):
     
     # Network inputs 
     ##rainfall image                                                                                                                                                                                           
@@ -106,7 +106,7 @@ def generator_initialized(gen, num_channels=1):
     return (model, noise_shapes)
 
 
-def discriminator(era_dim=(10,10,9), const_dim=(250,250,2), nimrod_dim=(250,250,1), filters=32, conv_size=(3,3), stride=1, relu_alpha=0.2, norm=None, dropout_rate=None):
+def discriminator(era_dim=(10,10,9), const_dim=(250,250,2), nimrod_dim=(250,250,1), filters=64, conv_size=(3,3), stride=1, relu_alpha=0.2, norm=None, dropout_rate=None):
     
     # Network inputs 
     ##rainfall image                                                                                                                                                                                         
