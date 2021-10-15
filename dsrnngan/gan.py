83e1cc80baba2c7da75e070bd5790bfdb9abf6a0
Andrew McRae <andrew.mcrae@physics.ox.ac.uk>
2021-10-15 11:38:19 +0000

    big bug fix?  generate new noise per batch, not per epoch
    Former-commit-id: 4ed1480358c99b36d55797c514aa7e0d6c9cf2f9

--- dsrnngan/gan.py
+++ dsrnngan/gan.py
@@ -175,7 +175,7 @@ class WGANGP(object):
                 
                 with Nontrainable(self.gen):   
                     dl = self.disc_trainer.train_on_batch(
-                        [cond,const,noise,sample], disc_target)
+                        [cond,const,noise(),sample], disc_target)
 
                 if disc_loss is None:
                     disc_loss = np.array(dl)
@@ -191,10 +191,10 @@ class WGANGP(object):
                 (cond, const, sample) = batch_gen_iter.get_next()
                 if self.mode == 'GAN':
                     gen_loss = self.gen_trainer.train_on_batch(
-                        [cond,const,noise], gen_target)
+                        [cond,const,noise()], gen_target)
                 elif self.mode == 'VAEGAN':
                     gen_loss = self.gen_trainer.train_step(
-                        [[cond,const,noise], gen_target])
+                        [[cond,const,noise()], gen_target])
                 del sample, cond, const
                 
             if show_progress:
