694b4ba170e815ac49f4338401b99423cb868186
ljharris23 <lucy-harris@live.co.uk>
2021-10-27 09:43:29 +0000

    removing print statements

--- dsrnngan/evaluation.py
+++ dsrnngan/evaluation.py
@@ -480,7 +480,6 @@ def log_spectral_distance_batch(batch1, batch2):
 def calculate_rapsd_rmse(truth, pred):
     ## avoid producing inf values
     if truth.max() <= 0.1:
-        print("low rain image identified, not calculating RASPD")
         return None
     fft_freq_truth = rapsd(truth, fft_method=np.fft)
     fft_freq_pred = rapsd(pred, fft_method=np.fft)
