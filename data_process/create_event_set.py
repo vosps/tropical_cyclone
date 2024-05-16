2d8fbe13425460bf572a06ec0cc509732580f193
vosps <56444100+vosps@users.noreply.github.com>
2024-05-16 21:03:08 +0100

    more pop exposure

--- data_process/create_event_set.py
+++ data_process/create_event_set.py
@@ -9,19 +9,20 @@ import pandas as pd
 # import cartopy.crs as ccrs
 import warnings
 import xarray as xr
-
 from utils.evaluation import find_landfalling_tcs,tc_region,create_xarray,get_storm_coords
-
 from utils.data import load_tc_data
 from utils.plot import make_cmap
 # import xesmf as xe
+import numpy.ma as ma
 # import glob
 import cftime as cf
 import os
+from multiprocessing import Pool, cpu_count, set_start_method
 warnings.filterwarnings("ignore")
 sns.set_style("white")
 sns.set_palette(sns.color_palette("Paired"))
 sns.set_palette(sns.color_palette("Set2"))
+from global_land_mask import globe
 
 # TODO: check which way the storms are rotating and how this is plotted - if using imshow it won't take into account
 # the fact that mswep uses reverse latitude
@@ -347,6 +348,73 @@ def globalise_storm_rain(storm,prediction=True):
 			grid_rain[t,sel[1],sel[0]] = storm_rain
 	return grid_rain
 
+def extremise_storm_rain(storm,prediction=True):
+	ntime,_,_,_ = storm.precipitation.shape
+
+	# define global extent
+	fp = '/bp1/geog-tropical/data/Obs/MSWEP/3hourly_invertlat/2000342.00.nc'
+	d = Dataset(fp, 'r')
+	lat = d.variables['lat'][:] #lat
+	lon = d.variables['lon'][:] #lon
+	
+	grid_x, grid_y = np.meshgrid(lon,lat)
+	z = globe.is_land(grid_y,grid_x)
+
+	# superimpose rain onto global grid
+	if prediction == True:
+		grid_rain = np.zeros((ntime,grid_x.shape[0],grid_y.shape[1],20))
+	else:
+		grid_rain = np.zeros((ntime,grid_x.shape[0],grid_y.shape[1]))
+		land_maxs = np.zeros((ntime))
+
+	for t in range(ntime):
+		storm_lons = storm.storm_lons[t,:,:]
+		storm_lats = storm.storm_lats[t,:,:]
+
+		Mlon = storm_lons[-1,-1]
+		mlon = storm_lons[0,0]
+		Mlat = storm_lats[-1,-1]
+		mlat = storm_lats[0,0]
+		if mlon.values > Mlon.values:
+			continue
+
+		if Mlon.values < mlon.values:
+			print('longitude not monotonically increasing')
+
+		Xspan = np.where((grid_x <= Mlon.values) & (grid_x >= mlon.values))[1][[0, -1]]
+		Yspan = np.where((grid_y <= Mlat.values) & (grid_y >= mlat.values))[0][[0, -1]] #adding in .values here to see if that fixes things
+
+		# Create a selection
+		sel = [slice(Xspan[0], Xspan[1] + 1), slice(Yspan[0], Yspan[1] + 1)]
+
+		if prediction == True:
+			for i in range(20):
+				storm_rain = storm.precipitation[t,:,:,i]
+				
+				grid_rain[t,sel[1],sel[0],i] = storm_rain
+		else:
+			storm_rain = storm.precipitation[t,:,:,0]
+			# print('max storm rain',np.max(storm_rain.values))
+
+			if grid_rain[t,sel[1],sel[0]].shape != (100,100):
+				print("wrong grid size")
+				continue
+
+			grid_rain[t,sel[1],sel[0]] = storm_rain
+		
+	
+	
+	# does any grid square get above threshold?
+	grid_rain_hrly = grid_rain / 3
+	l_rain = (grid_rain_hrly * z).flatten()
+	land_rain = l_rain[l_rain > 0]
+	grid_rain_max = np.max(grid_rain_hrly,axis=0)
+	# print('land_max_is:', land_rain.shape)
+	# how many times?
+	grid_rain_threshold_count = (grid_rain_hrly > 16.2).sum(axis=0)
+
+	return grid_rain_max,grid_rain_threshold_count,land_rain
+
 def find_dates(sids,model_cal,scenario):
 
 	dates = sids.apply(lambda row : cf.datetime(calendar=model_cal,
@@ -357,9 +425,45 @@ def find_dates(sids,model_cal,scenario):
 									), axis=1)
 	return dates
 
+def process_storm(storm_filename):
+	"""Process a single storm file."""
+	if '.nc' in storm_filename:
+		print(storm_filename)
+		storm = xr.open_dataset(tc_dir + storm_filename)
+		# print('storm 0',np.sum(storm.precipitation))
+		storm_rain,threshold_count,storm_maxs = extremise_storm_rain(storm, prediction=False) # in units mm/hour
+		# filter for extreme rain greater than 50mm/hour
+		# print('storm_rain1',np.sum(storm_rain))
+		# storm_rain[storm_rain <= 16.2] = 0
+		# print('storm_rain2',np.sum(storm_rain))
+		# print('storm threshold count',np.sum(threshold_count))
+		
+		return storm_rain,threshold_count,storm_maxs
+	
+def process_file(args):
+	"""Process a single file and return the result."""
+	file,tc_index = args
+	# global global_extreme_rain
+	result = process_storm(file)
+	# print(result)
+	if result is not None:
+		rain,count,maxs = result
+		# if result is not None:
+		# print('assigning data to storm ',tc_index)
+		file_dir = f'/user/home/al18709/work/event_sets/processing/{model}_{scenario}/'
 
+		# global_extreme_rain[tc_index, :, :] = result
+		np.save(f'{file_dir}{tc_index}_rain.npy',rain)
+		np.save(f'{file_dir}{tc_index}_count.npy',count)
+		np.save(f'{file_dir}{tc_index}_max.npy',maxs)
+		print('assigning data to storm ',tc_index)
+		# print('rain',np.sum(rain))
+		# print('count',np.sum(count))
+	else:
+		print(f'result is None, index = {tc_index}')
+	
 
-print('chips')
+# print('chips')
 
 # ['canesm','cnrm6','ecearth6','ipsl6','miroc6','mpi6','mri6','ukmo']
 # 'canesm'#'cnrm6'#'ecearth6'#'ipsl6'#'miroc6'#'ukmo'#'mpi6' #'mri6'
@@ -367,84 +471,261 @@ print('chips')
 # ipsl6 calendar is not standard
 # canesm calendar is not standard
 # https://loca.ucsd.edu/loca-calendar/
-# ukmo, mri6, ecearth6, miroc6,cnrm6,canesm,ipsl6
-model = 'mpi6'
+# ukmo, mri6, ecearth6, miroc6,cnrm6,canesm,ipsl6,mpi6
+model = 'cnrm6'
 cal = 'standard'
 # cal = 'proleptic_gregorian'
 print(model)
 scenario = 'ssp245'
-if scenario == 'hist':
-	yr1 = 2000
-	yr2 = 2014
-else:
-	yr1 = 2085
-	yr2 = 2099
 print(scenario)
-data = np.load(f'/user/home/al18709/work/ke_track_rain/hr/{model}_{scenario}_pred_qm.npy')
-meta = pd.read_csv(f'/user/home/al18709/work/ke_track_inputs/{model}_{scenario}_tracks.csv')
-meta.lon[meta.lon > 180] = meta.lon[meta.lon > 180] - 360
-meta2 = pd.DataFrame({'sid':meta.sid ,'centre_lat':meta.lat, 'centre_lon':meta.lon, 'hour':meta.hour, 'day':meta.day,'month':meta.month, 'year':meta.year})
 
-condition1 = (meta2.year >= yr1) & (meta2.year <= yr2)
-rain = data[condition1]
-meta3 = meta2[condition1].reset_index()
-# rain2 = rain[meta3.year <= yr2]
-# meta4 = meta3[meta3.year <= yr2].reset_index()
+# # 1. globalise storms
+# if scenario == 'hist':
+# 	yr1 = 2000
+# 	yr2 = 2014
+# else:
+# 	yr1 = 2085
+# 	yr2 = 2099
+# print(scenario)
+# data = np.load(f'/user/home/al18709/work/ke_track_rain/hr/{model}_{scenario}_pred_qm.npy')
+# meta = pd.read_csv(f'/user/home/al18709/work/ke_track_inputs/{model}_{scenario}_tracks.csv')
+# meta.lon[meta.lon > 180] = meta.lon[meta.lon > 180] - 360
+# meta2 = pd.DataFrame({'sid':meta.sid ,'centre_lat':meta.lat, 'centre_lon':meta.lon, 'hour':meta.hour, 'day':meta.day,'month':meta.month, 'year':meta.year})
+# condition1 = (meta2.year >= yr1) & (meta2.year <= yr2)
+# rain = data[condition1]
+# meta3 = meta2[condition1].reset_index()
+# # rain2 = rain[meta3.year <= yr2]
+# # meta4 = meta3[meta3.year <= yr2].reset_index()
+# condition2 = (meta3.month !=2 ) & (meta3.day != 30)
+# rain_cond = rain[condition2]
+# meta_cond = meta3[condition2].reset_index()
+# meta5 = pd.DataFrame({'sid':meta_cond.sid ,'centre_lat':meta_cond.centre_lat, 'centre_lon':meta_cond.centre_lon,'date':find_dates(meta_cond,cal,scenario)})
+# path = f'/user/home/al18709/work/event_sets/wgan_{model}_{scenario}/'
+# if os.path.exists(path):
+# 	print(f'{path} exists!')
+# else:
+# 	os.makedirs(path)
+# mode = 'chips'
+# save_event_set(meta5,rain_cond,path,mode,ens=False)
 
-condition2 = (meta3.month !=2 ) & (meta3.day != 30)
 
-rain_cond = rain[condition2]
-meta_cond = meta3[condition2].reset_index()
 
-meta5 = pd.DataFrame({'sid':meta_cond.sid ,'centre_lat':meta_cond.centre_lat, 'centre_lon':meta_cond.centre_lon,'date':find_dates(meta_cond,cal,scenario)})
-path = f'/user/home/al18709/work/event_sets/wgan_{model}_{scenario}/'
-if os.path.exists(path):
-	print(f'{path} exists!')
-else:
-	os.makedirs(path)
-mode = 'chips'
-save_event_set(meta5,rain_cond,path,mode,scenario,ens=False)
-
-print('event set saved',path)
-
+# 2. new threshold
 tc_dir = f'/user/home/al18709/work/event_sets/wgan_{model}_{scenario}/'
-tc_files = np.sort(os.listdir(tc_dir))
-global_rain = np.zeros((1800,3600))
-for storm_filename in tc_files:
-	if ('.nc' in storm_filename):
-		print(storm_filename)
-		storm = xr.open_dataset(tc_dir + storm_filename)
-		storm_rain = globalise_storm_rain(storm,prediction=False)
-		global_rain = global_rain + np.sum(storm_rain,axis=0)
-np.save(f'/user/home/al18709/work/event_sets/wgan_{model}_{scenario}/{model}_{scenario}_accumulated_global.npy',global_rain)
+# tc_files = np.sort(os.listdir(tc_dir))[:]
+# tc_files = tc_files['.nc' in tc_files]
+tc_files1 = np.array(np.sort(os.listdir(tc_dir))[:])
+tc_files2 = ['.nc' in file for file in tc_files1]
+tc_files = tc_files1[tc_files2][:]
+
+# Initialize global variables
+nstorms = sum('.nc' in file for file in tc_files)
+print('number of storms:',nstorms)
+
+# get ready to save a bunch of storm files
+file_dir = f'/user/home/al18709/work/event_sets/processing/{model}_{scenario}/'
+if os.path.exists(file_dir):
+	print(f'{file_dir} exists!')
+	# empty old storms
+	# files = os.listdir(file_dir)
+	# for f in files:
+	# 	os.remove(f'{file_dir}{f}')
+	# remove any files we already did
+	processed_files = os.listdir(file_dir)
+	import re
+	regex = r"(.+?)_rain.npy"
+	remove = []
+	for f in processed_files:
+		# print(f)
+		match = re.match(regex, f)
+		# print(match)
+		if match:
+			file_number = int(match.group(1))
+			remove.append(file_number)
+else:
+	os.makedirs(file_dir)
+	remove = []
+
+
+file_indices = [(file, idx) for idx, file in enumerate(tc_files)]
+file_indices = [val for idx, val in enumerate(file_indices) if idx not in remove]
+print('this many storms still to do:',len(file_indices))
+
+# split up based on number of samples we select
+import sys
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+if b == 2000:
+	file_indices = file_indices[a:]
+else:
+	file_indices = file_indices[a:b]
+print('this many storms still to do in this script:',len(file_indices))
+
+# Use multiprocessing to parallelize the processing
+if __name__ == '__main__':
+	set_start_method("fork", force=True)
+	pool = Pool(6)
+	# split = np.array_split(file_indices, 24)
+	pool.map(process_file, file_indices)
+	pool.close()
+	pool.join()
+print('results pooled!')
+# exit()
+exit()
+# go through rain arrays and calculate mean
+# import gc
+# gc.collect()
+global_extreme_rain = np.zeros((nstorms, 1800, 3600))
+global_extreme_count = np.zeros((nstorms, 1800, 3600))
+files = os.listdir(file_dir)
+max_rains = []
+for i,_ in enumerate(tc_files):
+	print(i)
+	
+	try:
+		rain = np.load(f'{file_dir}{i}_rain.npy')
+	except:
+		print(f'{file_dir}{i}_rain.npy')
+		continue
+	try:
+		rain_count = np.load(f'{file_dir}{i}_count.npy')
+	except:
+		print(f'{file_dir}{i}_count.npy')
+		continue
+	try:
+		storm_maxs = np.load(f'{file_dir}{i}_max.npy')
+	except:
+		continue
+	global_extreme_rain[i,:,:] = rain
+	global_extreme_count[i,:,:] = rain_count
+	max_rains.append([m for m in storm_maxs])
+
+print(global_extreme_rain)
+global_mean_rain = np.max(global_extreme_rain,axis=0)
+global_count = np.sum(global_extreme_count,axis=0)
+print(np.sum(global_mean_rain))
+np.save(f'/user/home/al18709/work/event_sets/wgan_{model}_{scenario}/{model}_{scenario}_mean_extreme_global.npy',global_mean_rain)
+np.save(f'/user/home/al18709/work/event_sets/wgan_{model}_{scenario}/{model}_{scenario}_count_extreme_global.npy',global_count)
+np.save(f'/user/home/al18709/work/event_sets/wgan_{model}_{scenario}/{model}_{scenario}_max_rain_over_land.npy',storm_maxs)
+print('datasets saved!')
+print(f'/user/home/al18709/work/event_sets/wgan_{model}_{scenario}/{model}_{scenario}_count_extreme_global.npy')
+print(f'/user/home/al18709/work/event_sets/wgan_{model}_{scenario}/{model}_{scenario}_mean_extreme_global.npy')
+print(f'/user/home/al18709/work/event_sets/wgan_{model}_{scenario}/{model}_{scenario}_max_rain_over_land.npy')
 
 
 exit()
-print('mswep')
-mswep_rain = np.expand_dims(np.load('/user/home/al18709/work/CMIP6/MSWEP/storm_rain/storm_rain_tcs_and_ts.npy'),axis=-1)
-mswep_lats = np.load('/user/home/al18709/work/CMIP6/MSWEP/storm_rain/storm_lats_tcs_and_ts.npy')
-mswep_lons = np.load('/user/home/al18709/work/CMIP6/MSWEP/storm_rain/storm_lons_tcs_and_ts.npy')
-print(mswep_lats.shape)
-print(mswep_lons.shape)
-meta = pd.read_csv('/user/home/al18709/work/CMIP6/MSWEP/storm_rain/storm_sid_tcs_and_ts.csv')
-mswep_meta = pd.DataFrame({'sid':meta.sid ,'centre_lat':meta.centre_lat, 'centre_lon':meta.centre_lon,'date':find_dates(meta,'standard')})
-path = '/user/home/al18709/work/event_sets/MSWEP/'
-mode = 'mswep'
-save_event_set(mswep_meta,mswep_rain,path,mode,ens=False)
+# # 3. accumulated threshold
+# global_rain = np.zeros((1800,3600))
+# for storm_filename in tc_files:
+# 	if ('.nc' in storm_filename):
+# 		print(storm_filename)
+# 		storm = xr.open_dataset(tc_dir + storm_filename)
+# 		storm_rain = globalise_storm_rain(storm,prediction=False)
+# 		global_rain = global_rain + np.sum(storm_rain,axis=0)
+
+# np.save(f'/user/home/al18709/work/event_sets/wgan_{model}_{scenario}/{model}_{scenario}_accumulated_global.npy',global_rain)
+
+
+
+# print('mswep')
+# mswep_rain = np.expand_dims(np.load('/user/home/al18709/work/CMIP6/MSWEP/storm_rain/storm_rain_tcs_and_ts.npy'),axis=-1)
+# mswep_lats = np.load('/user/home/al18709/work/CMIP6/MSWEP/storm_rain/storm_lats_tcs_and_ts.npy')
+# mswep_lons = np.load('/user/home/al18709/work/CMIP6/MSWEP/storm_rain/storm_lons_tcs_and_ts.npy')
+# print(mswep_lats.shape)
+# print(mswep_lons.shape)
+# meta = pd.read_csv('/user/home/al18709/work/CMIP6/MSWEP/storm_rain/storm_sid_tcs_and_ts.csv')
+# mswep_meta = pd.DataFrame({'sid':meta.sid ,'centre_lat':meta.centre_lat, 'centre_lon':meta.centre_lon,'date':find_dates(meta,'standard')})
+# path = '/user/home/al18709/work/event_sets/MSWEP/'
+# mode = 'mswep'
+# save_event_set(mswep_meta,mswep_rain,path,mode,ens=False)
+
+# 2
+model = 'mswep'
+scenario = 'obs'
+
+# get ready to save a bunch of storm files
+file_dir = f'/user/home/al18709/work/event_sets/processing/{model}_{scenario}/'
+if os.path.exists(file_dir):
+	print(f'{file_dir} exists!')
+	# empty old storms
+	files = os.listdir(file_dir)
+	for f in files:
+		os.remove(f'{file_dir}{f}')
+else:
+	os.makedirs(file_dir)
+
 
 tc_dir = '/user/home/al18709/work/event_sets/MSWEP/'
-tc_files = os.listdir(tc_dir)
-global_rain = np.zeros((1800,3600))
-for storm_filename in tc_files:
-	if ('.nc' in storm_filename):
-		print(storm_filename)
-		storm = xr.open_dataset(tc_dir + storm_filename)
-		storm_rain = globalise_storm_rain(storm,prediction=False)
-		global_rain = global_rain + np.sum(storm_rain,axis=0)
-np.save('/user/home/al18709/work/event_sets/MSWEP/accumulated_global.npy',global_rain)
+tc_files1 = np.array(np.sort(os.listdir(tc_dir))[:])
+tc_files2 = ['.nc' in file for file in tc_files1]
+tc_files = tc_files1[tc_files2][:]
+
+# Initialize global variables
+nstorms = sum('.nc' in file for file in tc_files)
+print('number of storms:',nstorms)
+global_extreme_rain = np.zeros((nstorms, 1800, 3600))
+global_extreme_count = np.zeros((nstorms, 1800, 3600))
+
+# get ready to save a bunch of storm files
+file_dir = f'/user/home/al18709/work/event_sets/processing/{model}_{scenario}/'
+if os.path.exists(file_dir):
+	print(f'{file_dir} exists!')
+	# empty old storms
+	files = os.listdir(file_dir)
+	for f in files:
+		os.remove(f'{file_dir}{f}')
+else:
+	os.makedirs(file_dir)
+
+file_indices = [(file, idx) for idx, file in enumerate(tc_files)]
+
+# Use multiprocessing to parallelize the processing
+if __name__ == '__main__':
+	set_start_method("fork", force=True)
+	pool = Pool(24)
+	# split = np.array_split(file_indices, 24)
+	pool.map(process_file, file_indices)
+	pool.close()
+	pool.join()
+print('results pooled!')
+
+# go through rain arrays and calculate mean
+files = os.listdir(file_dir)
+max_rains = []
+for i,_ in enumerate(tc_files):
+	rain = np.load(f'{file_dir}{i}_rain.npy')
+	rain_count = np.load(f'{file_dir}{i}_count.npy')
+	storm_maxs = np.load(f'{file_dir}{i}_max.npy')
+	global_extreme_rain[i,:,:] = rain
+	global_extreme_count[i,:,:] = rain_count
+	max_rains.append([m for m in storm_maxs])
+
+global_mean_rain = np.max(global_extreme_rain,axis=0)
+global_count = np.sum(global_extreme_count,axis=0)
+print(np.sum(global_mean_rain))
+np.save(f'/user/home/al18709/work/event_sets/{model}_{scenario}/{model}_{scenario}_mean_extreme_global.npy',global_mean_rain)
+np.save(f'/user/home/al18709/work/event_sets/{model}_{scenario}/{model}_{scenario}_count_extreme_global.npy',global_count)
+np.save(f'/user/home/al18709/work/event_sets/{model}_{scenario}/{model}_{scenario}_max_rain_over_land.npy',storm_maxs)
+print('datasets saved!')
+print(f'/user/home/al18709/work/event_sets/{model}_{scenario}/{model}_{scenario}_count_extreme_global.npy')
+print(f'/user/home/al18709/work/event_sets/{model}_{scenario}/{model}_{scenario}_mean_extreme_global.npy')
+print(f'/user/home/al18709/work/event_sets/{model}_{scenario}/{model}_{scenario}_max_rain_over_land.npy')
+
+
+# # 3
+# tc_dir = '/user/home/al18709/work/event_sets/MSWEP/'
+# tc_files = os.listdir(tc_dir)
+# global_rain = np.zeros((1800,3600))
+# for storm_filename in tc_files:
+# 	if ('.nc' in storm_filename):
+# 		print(storm_filename)
+# 		storm = xr.open_dataset(tc_dir + storm_filename)
+# 		storm_rain = globalise_storm_rain(storm,prediction=False)
+# 		global_rain = global_rain + np.sum(storm_rain,axis=0)
+# np.save('/user/home/al18709/work/event_sets/MSWEP/accumulated_global.npy',global_rain)
 
 # TODO: check longitude goes between -180 and 180 not 0-360.
-
+exit()
 
 # # load scalar wgan data
 # real,inputs,rain,meta = load_tc_data(set='test',results='ke_tracks')
